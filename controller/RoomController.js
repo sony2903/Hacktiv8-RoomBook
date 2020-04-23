780a34f768b9cc64e9de83eaae16fab89d354f11
Samirudin242 <samtheyaku@gmail.com>
2020-04-23 21:38:32 +0700

    add button

--- controller/RoomController.js
+++ controller/RoomController.js
@@ -14,6 +14,74 @@ class RoomController {
             res.send(err)
         })
     }
+    static addRooms(req,res){
+        const alert = req.query
+        Room.findAll()
+        .then(data=> {
+            res.render('addRoom', {data,alert})
+        })
+        .catch( error=> {
+            res.send(error)
+        })
+        
+    }
+    
+    static addRoomsPost(req,res){
+        Room.create({
+            name: req.body.name,
+            capacity : Number(req.body.capacity)
+        })
+        .then(() => {
+            const msg = `Room : ${req.body.name} has been added`
+            res.redirect(`/Room?msg=${msg}`)})
+        .catch(err=>{
+            res.send(err)
+        })
+    }
+
+    static editRoom(req,res){
+        const alert = req.query
+        let dataPH
+       Room.findAll()
+        .then( data => {
+            dataPH = data
+            return Room.findByPk(Number(req.params.id))
+        })
+        .then(data => {
+            res.render('editRoom',{data,dataPH,alert})
+        })
+        .catch(err=>{
+            res.send(err)
+        })
+    }
+
+    static editRoomPost(req,res){
+        Room.update({
+            name: req.body.name,
+            capacity : Number(req.body.capacity)
+        },{
+            where: {id : Number(req.params.id)}
+        })
+        .then(()=> {
+            const msg = `Room ${req.body.name} has been edited`
+            res.redirect(`/Room?msg=${msg}`)})
+        .catch(err => {
+            res.send(err)
+        })
+    }
+
+    static deleteRoom(req,res){
+        Room.destroy({
+            where : {id : Number(req.params.id)}
+        })
+        .then(() => {
+            const msg = `Room has been deleted`
+            res.redirect(`/Room?msg=${msg}`)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    }
 }
 
 module.exports = RoomController
--- routes/room.js
+++ routes/room.js
@@ -3,6 +3,11 @@ const RoomController = require('../controllerr/RoomController')
 
 
 routers.get('/', RoomController.showRoom)
+routers.get('/add', RoomController.addRooms)
+routers.post('/add', RoomController.addRoomsPost)
+routers.get('/edit/:id', MovieController.editRoom)
+routers.post('/edit/:id', MovieController.editRoomPost)
+routers.get('/delete/:id', MovieController.deleteRoom)
 
 
 module.exports = routers
